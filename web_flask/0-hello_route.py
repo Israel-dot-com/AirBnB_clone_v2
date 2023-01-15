2fa378c665350ae4562af7a0c11216c1a2457744
Israel <isltimi1000@gmail.com>
2023-01-15 12:08:40 +0000

    Validate Pycode

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 """ A script that starts a Flask web application
+
 It must be listening on port 500, 0.0.0.0
-It has one route, '/' that displayes 'Hello HBNB'
+It has one route, 
+    '/' that displayes 'Hello HBNB'
 """
 from flask import Flask
 
 app =  Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
-    """'Hello HBNB'"""
+    """Function to display 'Hello HBNB!'"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
