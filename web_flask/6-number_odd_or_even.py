a2b20c3dcfbbac2825eb32a497f94f7f7469d410
Israel <isltimi1000@gmail.com>
2023-01-15 23:05:22 +0000

    Task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -5,36 +5,46 @@ Routes:
     /: Displays 'Hello HBNB!'.
     /hbnb: Displays 'HBNB'.
     /c/<text>: Displays 'C' followed by the value of <text>.
+    /python/(<text>): Displays 'Python' followed by the value of <text>.
+    /number/<n>: Displays 'n is a number' only if <n> is an integer.
+    /number_template/<n>: Displays an HTML page only if <n> is an integer.
+        - Displays the value of <n> in the body.
+    /number_odd_or_even/<n>: Displays an HTML page only if <n> is an integer.
+        - States whether <n> is even or odd in the body.
 """
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
 
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'."""
+    """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'."""
+    """Displays 'HBNB'"""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    """Displays 'C' followed by the value of <text>."""
+def c_text(text):
+    """Displays 'C' followed by the value of <text>
+    Replaces any underscores in <text> with slashes.
+    """
     text = text.replace("_", " ")
     return "C {}".format(text)
 
 
-@app.route("/python/", strict_slashes=False)
+@app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python_text(text="is cool"):
-    """Displays 'Python' followed by the value of <text>.
+    """Displays 'Python' followed by the value of <text>
     Replaces any underscores in <text> with slashes.
     """
     text = text.replace("_", " ")
@@ -43,11 +53,12 @@ def python_text(text="is cool"):
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def number_n(n):
-    """Displays 'n' is a number only if n is an integer"""
+    """Displays 'n is a number' only if <n> is an integer."""
     return "{} is a number".format(n)
 
+
 @app.route("/number_template/<int:n>", strict_slashes=False)
-def number_template(n):
+def number_n_template(n):
     """Displays an HTML page only if <n> is an integer.
     Displays the value of <n> in the body.
     """
