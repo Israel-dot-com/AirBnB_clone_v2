9201588dbf904e4b2052597a9dfddbd0a6dbbbdd
Israel <isltimi1000@gmail.com>
2023-01-15 21:22:22 +0000

    Add 6 peoper doc

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -53,9 +53,8 @@ def number_n_template(n):
 
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_n_odd_or_even(n):
-    """Displays a html page
-
-    + odd|even if n is an integer
+    """Displays an HTML page only if <n> is an integer.
+    States whether <n> is odd or even in the body.
     """
     return render_template("6-number_odd_or_even.html", n=n)
 
