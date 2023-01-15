766a7748634c119da16a04f4aaffe924546b824b
Israel <isltimi1000@gmail.com>
2023-01-15 18:59:57 +0000

    Add task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -39,10 +39,12 @@ def python_text(text="is cool"):
     text = text.replace("_", " ")
     return "Python {}".format(text)
 
+
 @app.route("/number/<int:n>", strict_slashes=False)
 def number_n(n):
-    """Displays 'n is a number' only if n is an integer."""
+    """Displays 'n' is a number only if n is an integer"""
     return "{} is a number".format(n)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
